f0a906709ca01095d64da038d3d3dee562fad4f9
pierluigi991 <123975572+pierluigi991@users.noreply.github.com>
2023-05-10 11:30:14 +0200

    lucas

--- src/esercizio3/RubricaTelefonica.java
+++ src/esercizio3/RubricaTelefonica.java
@@ -2,56 +2,94 @@ package esercizio3;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class RubricaTelefonica {
 
-	private static HashMap<String,String> rubrica = new HashMap<String,String>();
-	
-	
-	public void inserisciContatto(String nome, String numero) {
-		rubrica.put(nome, numero);
-	};
-	public void eliminaContatto(String nome) {
+	private Map<String, String> rubrica = new HashMap<>();
+
+	public void inserisciContatto(String nome, String telefono) {
+		rubrica.put(nome, telefono);
+	}
+
+	public void cancellaContatto(String nome) {
 		rubrica.remove(nome);
-	};
-	public void cercaTramiteNumero(String numero) {
-		for (String nome : rubrica.keySet()) {
-			if (rubrica.get(nome).equals(numero)) {
-				System.out.println(nome + ":" + numero);
+	}
+
+	public String cercaNome(String telefono) {
+		for (Map.Entry<String, String> entry : rubrica.entrySet()) {
+			if (entry.getValue().equals(telefono)) {
+				return entry.getKey();
 			}
 		}
-	};
-	public String cercaTramiteNome(String nome) {
-		return rubrica.get(nome);
-	};
-	public void stampaRubrica() {
-		System.out.println("Lista contatti: ");
-		for (String nome: rubrica.keySet()) {
-			String numero = rubrica.get(nome);
-			System.out.println(nome + ": " + numero);
+		return null;
+	}
 
+	public String cercaTelefono(String nome) {
+		return rubrica.get(nome);
+	}
+
+	public void stampaRubrica() {
+		for (Map.Entry<String, String> entry : rubrica.entrySet()) {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
+	}
+
+	public static void main(String[] args) {
+		RubricaTelefonica rubrica = new RubricaTelefonica();
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("\nScegli un'opzione:");
+			System.out.println("1. Aggiungi contatto");
+			System.out.println("2. Cancella contatto");
+			System.out.println("3. Cerca numero di telefono");
+			System.out.println("4. Cerca nome");
+			System.out.println("5. Stampa rubrica");
+			System.out.println("6. Esci");
+			System.out.print("Opzione: ");
+			int opzione = scanner.nextInt();
+			scanner.nextLine();
+
+			if (opzione == 1) {
+				System.out.print("Inserisci il nome: ");
+				String nome = scanner.nextLine();
+				System.out.print("Inserisci il numero di telefono: ");
+				String telefono = scanner.nextLine();
+				rubrica.inserisciContatto(nome, telefono);
+				System.out.println("Contatto aggiunto!");
+			} else if (opzione == 2) {
+				System.out.print("Inserisci il nome del contatto da cancellare: ");
+				String nome = scanner.nextLine();
+				rubrica.cancellaContatto(nome);
+				System.out.println("Contatto cancellato!");
+			} else if (opzione == 3) {
+				System.out.print("Inserisci il nome della persona di cui vuoi cercare il numero di telefono: ");
+				String nome = scanner.nextLine();
+				String telefono = rubrica.cercaTelefono(nome);
+				if (telefono != null) {
+					System.out.println("Numero di telefono di " + nome + ": " + telefono);
+				} else {
+					System.out.println("Contatto non trovato!");
+				}
+			} else if (opzione == 4) {
+				System.out.print("Inserisci il numero di telefono della persona di cui vuoi cercare il nome: ");
+				String telefono = scanner.nextLine();
+				String nome = rubrica.cercaNome(telefono);
+				if (nome != null) {
+					System.out.println("Nome della persona con il numero " + telefono + ": " + nome);
+				} else {
+					System.out.println("Contatto non trovato!");
+				}
+			} else if (opzione == 5) {
+				rubrica.stampaRubrica();
+			} else if (opzione == 6) {
+				break;
+			}
 		}
 
-	}
-	public static void main(String[] args) {
-
-		RubricaTelefonica rubrica = new RubricaTelefonica();
-
-		rubrica.inserisciContatto("athos", "25558689");
-		rubrica.inserisciContatto("porthos", "158788965");
-		rubrica.inserisciContatto("aramis", "15557476");
-	
-
-		rubrica.stampaRubrica();
-
-		rubrica.eliminaContatto("aramis");
-		rubrica.stampaRubrica();
-
-		rubrica.cercaTramiteNumero("15587896");
-		rubrica.cercaTramiteNome("athos");
-
+		scanner.close();
 	}
 
-	
-	
 }
+
